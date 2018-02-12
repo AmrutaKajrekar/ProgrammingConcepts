7a23cc00f32e6e9f43c98335182d87a5fa83aee8
212431401 <amruta.kajrekar@ge.com>
2018-02-12 10:55:28 -0800

    solved over the weekend

---
+++ src/com/main/contest/c71.java
@@ -0,0 +1,132 @@
+package com.main.contest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by 212431401 on 2/10/18.
+ */
+public class c71 {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+    public static void main(String[] args){
+        c71 obj = new c71();
+        //[4,2,6,1,3,null,null]
+//        TreeNode root = new TreeNode(4);
+//        TreeNode child1 = new TreeNode(2);
+//        TreeNode child2 = new TreeNode(6);
+//        TreeNode child3 = new TreeNode(1);
+//        TreeNode child4 = new TreeNode(3);
+//
+//        root.left = child1;
+//        root.right = child2;
+//        child1.left = child3;
+//        child1.right = child4;
+
+        //[27,null,34,null,58,50,44,null,null,null,null]
+//        27,null,34,null,58,50,null,44,null,null,null
+//        TreeNode root = new TreeNode(27);
+//        TreeNode child1 = new TreeNode(34);
+//        TreeNode child2 = new TreeNode(58);
+//        TreeNode child3 = new TreeNode(50);
+//        TreeNode child4 = new TreeNode(44);
+//
+//        root.right = child1;
+//        child1.right = child2;
+//        child2.left = child3;
+//        child3.left = child4;
+
+        TreeNode root = new TreeNode(90);
+        TreeNode child1 = new TreeNode(69);
+        TreeNode child2 = new TreeNode(49);
+        TreeNode child3 = new TreeNode(89);
+        TreeNode child4 = new TreeNode(52);
+
+        root.left = child1;
+        child1.right = child3;
+        child1.left = child2;
+        child2.right = child4;
+        System.out.println(obj.minDiffInBST(root));
+    }
+
+    public int minDiffInBST(TreeNode root) {
+        int min_Diff = Integer.MAX_VALUE;
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list = addToList(list, root);
+        Collections.sort(list);
+
+        for(int i=0;i<list.size()-1;i++){
+            int abs = Math.abs(list.get(i)-list.get(i+1));
+            if(abs<min_Diff) min_Diff=abs;
+        }
+        return min_Diff;
+    }
+
+    private ArrayList addToList(ArrayList list, TreeNode root) {
+        if(root!=null){
+            list.add(root.val);
+            addToList(list, root.left);
+            addToList(list, root.right);
+        }
+        return list;
+    }
+
+    public int minDiffInBST2(TreeNode root) {
+        int min_diff = Integer.MAX_VALUE;
+        //recursively call left and right tree nodes and find distance
+        int diff1 = findDist(root,root.left, min_diff);
+        int diff2 = findDist(root,root.right, min_diff);
+        if(diff1<diff2){
+            return diff1 <min_diff ? diff1: min_diff;
+        }
+        if(diff2<diff1){
+            return diff2 <min_diff ? diff2: min_diff;
+        }
+//        return min_diff;
+        return diff1 < diff2 ? diff1: diff2;
+    }
+
+    private int findDist(TreeNode root, TreeNode child, int min_diff) {
+        if(root!=null && child!=null){
+            int diff = Math.abs(root.val-child.val);
+            if(diff < min_diff){
+                min_diff=diff;
+            }
+            if(child.left!=null){
+                int diff3 =findDist(child,child.left,min_diff);
+                if ( diff3< min_diff){
+                    min_diff=diff3;
+                }
+            }
+            if(child.right!=null){
+                int diff4 =findDist(child,child.right,min_diff);
+                if ( diff4< min_diff){
+                    min_diff=diff4;
+                }
+            }
+        }
+        return min_diff;
+    }
+
+
+}
+ class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ src/com/main/leetcode/BitManipulation/TotalHammingDistance.java
@@ -0,0 +1,75 @@
+package com.main.leetcode.BitManipulation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author amruta.kajrekar
+ * leetcode problem #477
+ *
+ *The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+Now your job is to find the total Hamming distance between all pairs of the given numbers.
+
+Example:
+Input: 4, 14, 2
+Output: 6
+
+Explanation: In binary representation, the 4 is 0100, 14 is 1110, and 2 is 0010 (just
+showing the four bits relevant in this case). So the answer will be:
+HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+Note:
+Elements of the given array are in the range of 0 to 10^9
+Length of the array will not exceed 10^4
+ */
+
+public class TotalHammingDistance {
+    
+    public static void main(String[] args){
+        TotalHammingDistance hamming = new TotalHammingDistance();
+        System.out.println("final" +hamming.totalHammingDistance(new int[]{6,1,8,6,8}));
+    }
+
+    public int totalHammingDistance(int[] nums) {
+
+        if(nums.length <2) return 0;
+
+        int counter=0;
+        List<String> bitStr = new ArrayList<String>();
+        int max_size=0;
+        //convert all int to binary strings
+        for (int i=0;i<nums.length;i++){
+            String str= Integer.toBinaryString(nums[i]);
+            bitStr.add(str);
+            if(str.length()>max_size){
+                max_size=str.length();
+            }
+        }
+
+        //find the max size and make all String lengths equal
+        for(int i=0;i<bitStr.size();i++) {
+            StringBuilder sb = new StringBuilder();
+            if (bitStr.get(i).length() < max_size) {
+                while (sb.length() + bitStr.get(i).length() < max_size) {
+                    sb.append('0');
+                }
+            }
+            sb.append(bitStr.get(i));
+            bitStr.set(i,sb.toString());
+            System.out.println("Str= " + bitStr.get(i));
+        }
+
+        HammingDistance ham = new HammingDistance();
+
+        //iterate through the list and calculate haming distance for each of the 2 strings
+        // and keep the counter adding
+        if(bitStr.size()>2) {
+            for (int i = 0; i < bitStr.size() - 1; i++) {
+                int newCounter = ham.hammingDistance(bitStr.get(i), bitStr.get(i + 1));
+                counter += newCounter;
+                System.out.println("dist"+ newCounter);
+            }
+        }
+        counter+=ham.hammingDistance(bitStr.get(0),bitStr.get(bitStr.size()-1));
+        return counter;
+    }
+}
